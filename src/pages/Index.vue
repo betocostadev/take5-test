9460fb061e982a7317a2b6d868e5ff35735d78a6
betocostadev <robertomarx@gmail.com>
2020-01-09 15:28:00 -0300

    fetching pokemons data done

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>Pokedex</h2>
+  <q-page class="flex flex-center no-wrap">
+    <h2 class="text-h2">Pokedex</h2>
     <button v-on:click="loadData()">Show</button>
-    <button v-on:click="say(loadData())">Show2</button>
+    <div class="row">
+    <p class="text-center">Pokémon list</p>
     <ul id="example-1">
-      <li v-for="pokemon in pokemons" v-bind:key="pokemon.name">
-        {{ pokemon.name }}
+      <li v-for="(index, pokemon) in pokemons" v-bind:key="pokemon.url">
+        {{ index.name }} {{ index.url }}
       </li>
     </ul>
+    </div>
   </q-page>
 </template>
 
@@ -17,9 +19,22 @@
 Carregar os dados usando o "mounted". Usar um botão que irá colocar tudo na página, chamando os dados
 que já estarão carregados através do mounted. */
 
-import { date } from 'quasar'
 export default {
   name: 'PageIndex',
+  async created () {
+    const pokemons = await this.$axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40`)
+    console.log('pokemons are:', pokemons.data)
+    console.log('first poke:', pokemons.data.results[0])
+    pokemons.data.results.forEach(pokemon => {
+      console.log(pokemon.name)
+    })
+    this.pokemons = pokemons.data.results
+  },
+  data () {
+    return {
+      pokemons: []
+    }
+  },
   methods: {
     say: function (msg) {
       alert(msg)
@@ -33,22 +48,16 @@ export default {
         })
       return test
     }
-  },
-  computed: {
-    todaysDate () {
-      let timeStamp = Date.now()
-      return date.formatDate(timeStamp, 'YYYY-MM-DD')
-    }
-  },
-  mounted () {
-    this.$nextTick(function () {
-      const pokemons = this.$axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40')
-        .then((response) => {
-          const data = response.data.results
-          return data
-        })
-      return pokemons
-    })
   }
+  // mounted () {
+  //   this.$nextTick(function () {
+  //     const pokemons = this.$axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40')
+  //       .then((response) => {
+  //         const data = response.data.results
+  //         return data
+  //       })
+  //     return pokemons
+  //   })
+  // }
 }
 </script>
